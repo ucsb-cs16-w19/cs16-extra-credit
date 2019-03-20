12e519009fabcf3350c06d25e64e57cb76b3b75c
Zhou Zhuang <zhouzhuang@ucsb.edu>
2019-03-19 21:48:13 -0700

    a

--- README.md
+++ README.md
@@ -1,22 +1,9 @@
-# CS 16 Extra Credit Project
-Extra credit project that showcases the concepts learned in CS 16.
+#CS 16 extra-credit-project
+#Author Joe Zhuang
+#Professor YeKaterina Kharitonova
 
-To submit your project:
-* Fork this repo
-* Clone *your fork* of this repo
-* Make your changes in that repo
-* Commit your changes
-* Push your changes (to the forked repo that you cloned above)
+#Description:
+#A program that only contains plain code might be boring.
+#Inspired by lab02, I find it interesting to output the result
+#as images which consist of asterisks ('*')
 
-Whenever you are ready to submit your extra credit, submit a pull request through the GitHub.com interface (i.e., once you fork the project and make the changes, you can go to "Pull Requests" (PR) tab and submit a new PR (to the original repo)).
-
-If your PR is successful, you should be able to go to the original repo <https://github.com/ucsb-cs16-w19/cs16-extra-credit> and see your PR listed there under the "Pull Requests" tab.
-
----
-
-Make sure that you update the documentation inside `main.cpp`. If you have more than one file to submit,
-make sure that each file uses the same format and provides that documentation. Additionally, if you have
-several files, ensure that your Makefile allows us to properly compile each project.
-
-Feel free to update your repo's README to provide high-level overview of the project and your 
-motivation for creating it (other than a grade ;-)).
--- main.cpp
+++ main.cpp
@@ -2,31 +2,138 @@
 using namespace std;
 
 /*=============================================================================
- |     Author:    STUDENT'S NAME HERE
- |   To Compile:  EXPLAIN HOW TO COMPILE THIS PROGRAM 
- |
- |        Class:  NAME AND TITLE OF THE CLASS FOR WHICH THIS PROGRAM WAS
- |                      WRITTEN
- |    Concepts:   DESCRIBE THE CONCEPTS FROM THE COURSE THAT THIS PROGRAM USES
- |
+ |     Author:    Joe Zhuang
+ |   To Compile:  simply type ./main width height after compiling 
+ |                example:( ./main 4 6)
+ |        Class:  CS 16 :Problem Solving I. (Professor K.)
+ |    Concepts:   utilize loops and stars
+                  to output short sentences with images.
  +-----------------------------------------------------------------------------
  |
- |  Description:  DESCRIBE THE PROBLEM THAT THIS PROGRAM WAS WRITTEN TO
- |      SOLVE.
+ |  Description:  output CS16 with stars
  |
- |        Input:  DESCRIBE THE INPUT THAT THE PROGRAM REQUIRES.
+ |        Input:  width and height of each letter.
  |
- |       Output:  DESCRIBE THE OUTPUT THAT THE PROGRAM PRODUCES.
+ |       Output:  the image of letters
  |
- |    Algorithm:  OUTLINE THE APPROACH USED BY THE PROGRAM TO SOLVE THE
- |      PROBLEM.
+ |    Algorithm:  for loops
  |
- |   Known Bugs:  IF THE PROGRAM DOES NOT FUNCTION CORRECTLY IN SOME
- |      SITUATIONS, DESCRIBE THE SITUATIONS AND PROBLEMS HERE.
+ |   Known Bugs:  if the width and height of each letter are assigned to different values, the images of each letter will be abnormal.
+     letter 'S' looks like number '5'
+     hard to perform 'S' and '6' smoothly.
+     fail to output "CS16" in the same line.
  |
  *===========================================================================*/
+string starC(int width, int height){
+	string result = "";
+	if(width < 2 || height < 5){
+		return result;
+	}
+	for (int i = 0; i < width + 1; i++){
+		result += "*";
+	}
+	result += "\n";
+	for (int i = 0; i < height - 3; i++){
+		result += "*";
+		for ( int j = 0; j < width - 1; j++){
+			result += " ";
+		}
+		result += "\n";
+		
+	}
+	for (int i = 0; i < width + 1 ; i++){
+		result += "*";
+	}
+	result += "\n";
+	return result;
+	}
 
-int main () {
+string starS(int width, int height){
+	string result = "";
+	if (width < 3 || height < 5){
+		return result;
+	}
+	for (int i = 0; i < width; i++){
+		result += "*";
+	}
+	result += "\n";
+	for (int i = 0; i <= height/2 - 2; i ++){
+		result += "*";
+		result += "\n";
+	}
+	for (int i = 0 ; i < width; i++){
+		result += "*";
+	}
+	result += "\n";
+	for (int i = 0; i < width - 2 ; i++){
+		for (int j = 0; j < width; j++){
+			result += " ";
+		}
+		result += "*";
+		result += "\n";
 
+	}	
+	for (int i = 0; i < width ; i++){
+		result += "*";
+	}
+	result += "\n";
+	return result;
+	}
+
+string star1(int height){
+	string result = "";
+	if (height < 1){
+		return result;
+	}
+	for (int i = 0; i < height; i++){
+		result += "*";
+		result += "\n";
+	}
+	return result;
+}
+string star6(int width, int height){
+	string result = "";
+	if (width < 3 || height < 3){
+		return result;
+	}
+	for (int i = 0; i < width; i++){
+		result += "*";
+	}
+	result += "\n";
+	for (int i = 0; i < height - 4; i++){
+		result += "*";
+		result += "\n";
+	}
+	for ( int i = 0; i < width; i ++){
+		result += "*";
+	}
+	result += "\n";
+	for (int i = 0; i < width - 2; i++){
+		result += "*";
+		for (int j = 0; j < width - 2; j++){
+			result += " ";
+		}
+		result += "*";
+		result += "\n";
+	}
+	for ( int i = 0; i < width; i++){
+		result += "*";
+	}
+
+	return result;
+}
+
+
+int main (int argc, char* argv[]) {
+	if (argc != 3){
+		cerr << "please enter 3 arguments" << endl;
+		exit(1);
+	}
+	int width = stoi(argv[1]);
+	int height = stoi(argv[2]);
+	cout << starC(width, height) << endl;
+	cout << starS(width, height) << endl;
+	cout << star1(height) << endl;
+	cout <<star6(width, height) << endl;
     return 0;
 }
